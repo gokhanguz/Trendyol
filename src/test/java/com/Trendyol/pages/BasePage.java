bdcf2351f0944aa3ba22a280ef77de40204f9f55
gokhanguz <67508424+gokhanguz@users.noreply.github.com>
2021-02-20 16:36:29 +0300

    test

--- src/test/java/com/Trendyol/pages/BasePage.java
+++ src/test/java/com/Trendyol/pages/BasePage.java
@@ -1,9 +1,17 @@
 package com.Trendyol.pages;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import com.Trendyol.utilities.Driver;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public abstract class BasePage {
@@ -16,6 +24,23 @@ public abstract class BasePage {
 
     @FindBy(id="accountBtn")
     public WebElement girisYap;
+
+    public  void printToTxt(String info,String fileName) {
+        // you can add dynamic file name idForTxtFile
+        String idForTxtFile = new SimpleDateFormat("dd.MM.yyyy_HH.mm.ss").format(new Date());
+        File file = new File(fileName + idForTxtFile+".txt");
+        try {
+            FileWriter fw = new FileWriter(file, true);
+            //instead you could only use:
+            fw.write(info);
+            fw.flush();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println(e.getLocalizedMessage());
+        }
+    }
+
 
 
 
